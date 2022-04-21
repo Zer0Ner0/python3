f3f784ff9051b2c7449a047faf4f814098fc5b46
Amir Mazlan <amirmz48@gmail.com>
2022-04-21 04:35:30 +0000

    #Calculate age #Password Checker #Array/List

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print('Amir Mazlan')
+#Calculate age
+
+#name = 'Amir Mazlan'
+#birthday = input ("What year were you born?")
+#age = 2022 - int(birthday)
+
+#print(f'Your age is {age}')
+
+#---------------------------------------------------------------------------------------
+
+#Password Checker
+
+#uname = input('Your username is?')
+#pword = input('Your password is?')
+#pword_len = len(pword)
+#secret = '*' * pword_len
+
+#print(f'{uname}, your password {secret} is {pword_len} letters long')
+
+#---------------------------------------------------------------------------------------
+
+#Array/List
+
+string = 'hellooo'
+print(string[0:5:2])
+
+amazon_cart = [
+  'notebooks',
+  'sunglasses', 
+  'shoes',
+  'laptop',
+  'keyboard'
+]
+
+amazon_cart[0] = 'toys'
+new_cart = amazon_cart[:]
+new_cart[0] = 'table'
+
+print(new_cart)
+print(amazon_cart)
