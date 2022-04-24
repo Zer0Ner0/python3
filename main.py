714fa9aa89965c370e33628c894ae226829e7869
Amir Mazlan <amirmz48@gmail.com>
2022-04-24 20:01:23 +0000

    #Exercise All in One

--- main.py
+++ main.py
@@ -505,3 +505,15 @@
 # # ---------------------------------------------------------------------------------------
 
 #Exercise All in One
+
+# from translate import Translator
+
+# translator = Translator(to_lang='ms')
+# try:
+#     with open('./test.txt', mode='r') as my_file:
+#         text = my_file.read()
+#         translation = translator.translate(text)
+#         with open('./test-ms.txt', mode='w') as my_file2:
+#             my_file2.write(translation)
+# except FileNotFoundError as e:
+#     print('check your file path')
